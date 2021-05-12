6479a76aed3b9fa6c5a77ab22d04870cc9e33828
hpearce <hw312312@dal.ca>
2021-05-12 18:43:52 -0300

    feat(search): backend search functionality implemented
    basic case insensitive search by title, search results not visible yet

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,8 +1,10 @@
 function lookup(form) {
+  // extract the search value
   var searchValue = form.search.value;
-  fetch("http://howardpearce.ca/test").then( function (result) {
+  // send a request to the server to fetch the posts with a similar title.
+  fetch("http://howardpearce.ca/search_posts?search=" + searchValue).then( function (result) {
     result.json().then( function(text) {
-      console.log(text.text);
+      console.log(text.posts);
     })
   });
 }
