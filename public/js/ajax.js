3c44b4ef08453e482a563a6fa0fc82012f255e18
hpearce <hw312312@dal.ca>
2021-05-13 18:00:43 -0300

    feat(search): basic search results appear on screen, needs formatting

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -2,9 +2,22 @@ function lookup(form) {
   // extract the search value
   var searchValue = form.search.value;
   // send a request to the server to fetch the posts with a similar title.
-  fetch("http://howardpearce.ca/search_posts?search=" + searchValue).then( function (result) {
-    result.json().then( function(text) {
-      console.log(text.posts);
-    })
+  fetch("http://howardpearce.ca/search_posts?search=" + searchValue).then( result => {
+    result.json().then( response => {
+      // was anything returned?
+      if (Object.keys(response.posts).length > 0) {
+        console.log("results");
+        var postContainer = document.getElementById("post_search_container");
+        var innerHTML = "";
+        for (var post of response.posts) {
+          innerHTML += "<p>" + post.title + "</p>";
+        }
+        postContainer.innerHTML = innerHTML;
+      } else {
+        console.log("no results");
+      }
+    }).catch( error => {
+      // canned error message
+    });
   });
 }
