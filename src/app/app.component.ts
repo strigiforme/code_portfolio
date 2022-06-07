737c5f7168fbd2fb31a46dc7966b2d05b1c99c70
hpearce <hw312312@dal.ca>
2022-06-07 17:46:39 -0300

    feat: Add basic layout and component for navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+              '../assets/css/flex.css']
 })
 export class AppComponent {
   title = 'portfolio';
