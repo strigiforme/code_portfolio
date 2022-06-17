5977f25e386fddec84d64d39333636437f7d070f
hpearce <hw312312@dal.ca>
2022-06-17 11:46:05 -0300

    feat(UI): Add hero-section component
    Will contain the HTML and CSS for the initial view of the portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule,
